13437998b1db85cd66cd74105ae3be0e6b4d899e
aircat <43405467+zhangzhanbang@users.noreply.github.com>
2018-12-05 17:30:13 +0800

    Add files via upload

---
+++ lab5-1/snake_move.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define HEAD 'H'
+#define BODY 'X'
+#define BLANK ' '
+#define FOOD '$'
+#define WALL '*'
+
+#define REP(i, a, b) for (i = (a); i <= (b); ++i)
+#define DWN(i, a, b) for (i = (a); i >= (b); --i)
+
+const int N = 12; // 图边长 
+const int MAX_LEN = 20; // 蛇最大总长 
+
+char map[13][13]; // 图 
+int dx[5] = {0, 0, 0, -1, 1}; 
+int dy[5] = {0, -1, 1, 0, 0}; // 分别表示A，D，W，S 
+int x_snake[22], y_snake[22]; // 蛇每个部分的位置 
+int len_snake; //当前蛇长 
+
+void pre_build(); // 预建 
+
+void snake_Move(int move_typ); // move_typ ：移动类型 
+void show(); // 打印图 
+void gameover(); // 打印游戏结束 
+int gameover_check(); //判断游戏是否结束 
+
+int main()
+{
+	srand(time(NULL)); //设置初始随机种子 
+    pre_build();
+    show();
+    while (!gameover_check()) //游戏是否继续进行 
+    {
+        char ch = getchar(); // 读入命令 
+        switch (ch)
+        {
+	        case 'A': snake_Move(1); break ;
+   	    	case 'D': snake_Move(2); break ;
+        	case 'W': snake_Move(3); break ;
+        	case 'S': snake_Move(4); break ;
+        }
+        system("cls"); // 清屏 
+        show();
+    }
+	gameover(); //游戏结束 
+    return 0;
+}
+
+void pre_build()
+{
+	int i; 
+    REP(i, 1, N) 
+        map[1][i] = map[N][i] = WALL,
+        map[i][1] = map[i][N] = WALL,
+        map[i][N+1] = '\n';
+    //设置初始图 
+    
+    len_snake = 5; 
+    REP(i, 1, len_snake) 
+        x_snake[i] = 2,
+        y_snake[i] = 6-i+1,
+    //设置初始蛇的位置 
+    
+    map[x_snake[1]][y_snake[1]] = HEAD;
+    REP(i, 2, len_snake)
+        map[x_snake[i]][y_snake[i]] = BODY;
+    //在图上把蛇显示出来 
+}
+
+void show() // 打印当前图 
+{
+	int i, j; 
+    REP(i, 1, N)
+        REP(j, 1, N+1)
+            printf("%c", map[i][j]);
+}
+
+void snake_Move(int move_typ) 
+{
+	int i;
+	int t_x = x_snake[1] + dx[move_typ];
+	int t_y = y_snake[1] + dy[move_typ];
+	//t_x和t_y：蛇头移动后的位置 
+	
+	// 更新蛇的图上信息 
+	map[x_snake[len_snake]][y_snake[len_snake]] = BLANK; 
+	map[x_snake[1]][y_snake[1]] = BODY; 
+	map[t_x][t_y] = HEAD; // 设置新蛇头 
+	
+	    
+    // 更新蛇的位置信息 
+    DWN(i, len_snake, 2) //蛇身的位置等于它前一部分的位置 
+    {
+        x_snake[i] = x_snake[i-1];
+        y_snake[i] = y_snake[i-1];
+    }
+    x_snake[1] += dx[move_typ];
+    y_snake[1] += dy[move_typ];
+}
+
+int gameover_check() // 检查当前游戏状态 
+{
+	int i, j;
+    REP(i, 2, len_snake)
+        REP(j, 1, i-1)
+            if (x_snake[i] == x_snake[j]
+             && y_snake[i] == y_snake[j]) // 头撞到身体 
+                return 1;
+	REP(i, 1, len_snake)
+		if (x_snake[i] <= 1 || y_snake[i] <= 1
+		 || x_snake[i] >= 12  || y_snake[i] >= 12) //头撞到边界 
+			return 1;
+    return 0;
+}
+
+void gameover() //游戏结束 
+{ 
+    system("cls");
+    puts("Game Over!!!!"); 
+}
+
